de7481e8eaae312e637a41ea2b82af43cdbd878f
ANVRRT <albertonavarreteramirez@gmail.com>
2022-09-24 00:51:57 -0500

    Added json objects

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 from accounts.models import Account, Country
 from businesses.models import Business
 from api_keys.models import Api_Key
 
 # from rest_framework import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response  import Response
 from rest_framework import status
 
@@ -15,9 +19,13 @@ import secrets
 
 # Create your views here.
 
-class CreateAccount(View):
+# @csrf_exempt
+# @api_view(['GET', 'POST'])
+class CreateAccount(APIView):
     def post(self, request):
+        # print(request.data, request.headers)
         data = request.data
+
         customer_info = data["customer_info"]
         business_info = data.get("business_info", None)
 
