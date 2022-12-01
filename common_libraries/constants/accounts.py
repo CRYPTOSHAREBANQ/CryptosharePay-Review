7eff74d9a94cff7e31b49b54147655afe54f6bba
ANVRRT <albertonavarreteramirez@gmail.com>
2022-11-30 18:39:44 -0600

    adding account types

--- common_libraries/constants/accounts.py
+++ common_libraries/constants/accounts.py
@@ -1,6 +1,9 @@
 ACCOUNT_TYPES = {
     "NO_ACCOUNT",
-    "ACCOUNT"
+    "ACCOUNT",
+    "BUSINESS",
+    "INDIVIDUAL",
+    "TEST"
 }
 
 NO_ACCOUNT_REDIRECT_URL = "https://cryptosharepay.com/#payments"
